4f71444cbf9dba5c8dc8322f1043468a93b8a185
L1verp00ler <andrey.golubev.1991@mail.ru>
2016-10-10 22:14:10 +0300

    ДЗ №3: - адреса страниц заменены на "красивые" путем использования ЧПУ (+ добавлены контроллер и вьюха для вывода сообщения, если запрашиваемая страница не найдена).

--- www/views/news/add_news_form.php
+++ www/views/news/add_news_form.php
@@ -5,7 +5,7 @@
     <title>Стартовая страница</title>
 </head>
 <body>
-    <form action="/index.php/?ctrl=Admin&act=AddNews" method="post">
+    <form action="/admin/addNews" method="post">
         <label for="date">Дата добавления новости:</label><br>
         <input type="date" id="date" name="date" required><br>
         <label for="title">Заголовок новости:</label><br>

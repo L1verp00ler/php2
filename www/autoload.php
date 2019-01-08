424cab3f7daad708dd00de239af8a0b28622bc2a
L1verp00ler <andrey.golubev.1991@mail.ru>
2019-01-08 21:45:08 +0300

    # Проект * убрана старая автозагрузка

--- www/autoload.php
+++ www/autoload.php
@@ -2,21 +2,11 @@
 
 function __autoload($class)
 {
-    if (file_exists(__DIR__ . '/controllers/' . $class . '.php')) {
-        require __DIR__ . '/controllers/' . $class . '.php';
-    } elseif (file_exists(__DIR__ . '/models/' . $class . '.php')) {
-        require __DIR__ . '/models/' . $class . '.php';
-    } elseif (file_exists(__DIR__ . '/core/' . $class . '.php')) {
-        require __DIR__ . '/core/' . $class . '.php';
-    } elseif (file_exists(__DIR__ . '/views/' . $class . '.php')) {
-        require __DIR__ . '/views/' . $class . '.php';
-    } else {
-        $classParts = explode('\\', $class);
-        $classParts[0] = __DIR__;
-        $path = implode(DIRECTORY_SEPARATOR, $classParts) . '.php';
+    $classParts = explode('\\', $class);
+    $classParts[0] = __DIR__;
+    $path = implode(DIRECTORY_SEPARATOR, $classParts) . '.php';
 
-        if (file_exists($path)) {
-            require $path;
-        }
+    if (file_exists($path)) {
+        require $path;
     }
 }
